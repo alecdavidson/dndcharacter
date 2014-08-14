a5f289c755449b4b9e72372e578b21cb7c7073a1
Alec Davidson <aed93@nau.edu>
2014-08-13 22:22:45 -0700

    Update dndcharacter.py
    Updated dndcharacter.py

--- dndcharacter.py
+++ dndcharacter.py
@@ -1,6 +1,8 @@
+##Alec Davidson - Summer 2014
+
 import random, re
 
-def getRandomLine(rsv):
+def getRandomLine(rsv):                 # GetRandomLine code created my David. Those comments were his.
     file_h = open(rsv)                  #a file handle for the .txt in question
     limit = file_h.readline()           #the number of lines to search per the .txt
     limit = limit.replace('\n', '' )    #trim the return character
@@ -14,23 +16,58 @@ def getRandomLine(rsv):
     
     return(phrase)
 
-def magicCheck(Class, Alignment):
-    if 'Good' in Alignment:
-        return 'Sorcerer'
-    elif 'Evil' in Alignment:
-        return 'Warlock'
-    else:
-        return 'Wizard'
-
-def getChar():
-    Alignment = getRandomLine('alignment.txt')
-    Race = getRandomLine('race.txt')        #get a random pattern
-    Class = getRandomLine('class.txt')
-    if Class == 'MagicUser':
-        Class = magicCheck(Class, Alignment)
-    if Class == 'Paladin':
-        Alignment = 'Lawful Good'
-    Char = Alignment + "\n" + Race + "\n" + Class
-    return(Char)
+def classCount():                       # This returns the number classes available
+    file_h = open('class.txt')
+    count = file_h.readline()
     
-print(getChar())
+    return int(count)
+
+def bardbarianCheck(Alignment):         # Bards and Barbarians cannot be lawful, rechoose if they get a lawful alignment
+    if 'Lawful' in Alignment:
+        Alignment = getRandomLine('alignment.txt')
+        Alignment = bardbarianCheck(Alignment)
+    return Alignment
+
+def druidCheck(Alignment):              # Druids must have a neutral alignment, rechoose if they do not get a neutral alignment
+    if 'Neutral' not in Alignment:
+        Alignment = getRandomLine('alignment.txt')
+        Alignement = druidCheck(Alignment)
+    return Alignment
+
+def monkCheck(Alignment):               # Monks must have a lawful alignment, rechoose if they do not get a lawful alignment
+    if 'Lawful' not in Alignment:
+        Alignment = getRandomLine('alignment.txt')
+        Alignment = monkCheck(Alignment)
+    return Alignment
+
+def checkAlign(Class,Alignment):        # Run to check the alignment. If a conflict is found the alignment is repicked. Paladins are always lawful good.
+    if Class == 'Bard':
+        Alignment = bardbarianCheck(Alignment)
+    elif Class == 'Barbarian':
+        Alignment = bardbarianCheck(Alignment)
+    elif Class == 'Paladin':
+        Alignment = 'Lawful Good'
+    elif Class == 'Druid':
+        Alignment = druidCheck(Alignment)
+    elif Class == 'Monk':
+        Alignment = monkCheck(Alignment)
+    
+    return(Alignment)
+
+def getRace():                          # Randomly pick a race from the list
+    Race = getRandomLine('race.txt')
+    return Race
+
+def getClass():                         # Randomly pick a class from the list
+    Class = getRandomLine('class.txt')
+    return Class
+
+def getAlign(Class):                    # Randomly pick an alignment from the list
+    Alignment = getRandomLine('alignment.txt')
+    Alignment = checkAlign(Class, Alignment)
+    return Alignment
+
+def getTrait():                         # Randomly pick a trait from the list
+    trait = getRandomLine('traits.txt')
+    
+    return trait
