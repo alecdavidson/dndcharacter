77f83e447dae88eec44fcc12e0d61ae1ebe99288
Alec Davidson <aed93@nau.edu>
2014-08-19 22:53:52 -0700

    Create server.py
    For running Local Server

---
+++ server.py
@@ -0,0 +1,23 @@
+##Alec Davidson - Summer 2014
+import flask, flask.views, dnd_main
+
+app = flask.Flask(__name__)
+
+app.secret_key = "CorrectTurtleBatteryStaple"
+
+class View(flask.views.MethodView):
+  def get(self):
+    return flask.render_template('index.html')
+  
+  def post(self):
+	party = str(flask.request.form['party'])
+	reroll = str(flask.request.form['reroll'])
+
+	dnd_main.start(party,reroll)
+	return self.get()
+  
+app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET','POST'])
+
+app.debug = True
+app.run()
+
