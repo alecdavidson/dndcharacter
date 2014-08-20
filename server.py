5e008710ad2c2eae4672f475b00210fa4ff83f03
Alec Davidson <aed93@nau.edu>
2014-08-20 02:59:17 -0700

    Edits

--- server.py
+++ server.py
@@ -18,5 +18,7 @@ class View(flask.views.MethodView):
   
 app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET','POST'])
 
-app.run(debug = True, port=33507)
+port = int(os.environ.get('PORT', 5000))
+
+app.run(host='0.0.0.0', port=port)
 
