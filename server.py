2cbcf9678a457889f054ff1c50378829cea169b0
Alec Davidson <aed93@nau.edu>
2014-10-08 22:08:38 -0700

    removed secret key

--- server.py
+++ server.py
@@ -3,17 +3,16 @@ import flask, flask.views, os, dnd_main
 
 app = flask.Flask(__name__)
 
-app.secret_key = "CorrectTurtleBatteryStaple"
-
 class View(flask.views.MethodView):
   def get(self):
     return flask.render_template('index.html')
   
+  @property
   def post(self):
     party = str(flask.request.form['party'])
     reroll = str(flask.request.form['reroll'])
 
-    dnd_main.start(party,reroll)
+    dnd_main.start(party, reroll)
     return self.get()
   
 app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET','POST'])
