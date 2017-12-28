620f0a17336eeb056b40777a8b2893f55d36d774
Alec Davidson <alecdavidson@nau.edu>
2017-12-28 08:08:23 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -6,15 +6,15 @@ app = flask.Flask(__name__)
 app.secret_key = "GenericKey"  # figure out how to hide this or something idk probs not important for this project
 
 class View(flask.views.MethodView):
-    def get(self):
-        return flask.render_template('index.html')
+	def get(self):
+		return flask.render_template('index.html')
 
-    def post(self):
-        party = flask.request.form['party']
-        reroll = flask.request.form['reroll']
-
-        dnd_main.start(party, reroll)
-        return self.get()
+	def post(self):
+		party = flask.request.form['party']
+		reroll = flask.request.form['reroll']
+		fate = flask.request.form['fate']
+		dnd_main.start(party, reroll, fate)
+		return self.get()
   
 app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET','POST'])
 
