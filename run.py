c02d12f48464f4c97961eeb677dac5fc66df4594
Alec Davidson <alecedavidson@gmail.com>
2020-01-13 10:32:51 -0700

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,9 @@ from server import app
 
 port = int(os.environ.get('PORT', 33507))
 
+@app.route('/.well-known/pki-validation/AC0212261B5BDB1C7517E4C11DB3C970.txt')
+def showhash():
+	return 'EBCB539DC61ACC1556AA522E17F961336102B3CCA259507C64DACFE29A2F0BDC comodoca.com'
+	
 app.debug = True
 app.run(host='0.0.0.0', port=port)
